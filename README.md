420c9c8529fcc3d9a2a234a360a2ef9e32c85014
shaojiemike <shaojiemike@mail.ustc.edu.cn>
2024-09-05 10:01:54 +0800

    feat: add backward-cpp and icecream-printer BUT NO CHECK

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # cpp-printer
+
+## Motivation
+
+* Test/Run cases is a good way to understand the project logic.
+* Multi-Process/Thread Situation
+    * GDB is difficult to used 
+    * cout > terminal is in chaos
+
+## Features
+
+* [x] Integrated with [icecream-cpp](https://github.com/renatoGarcia/icecream-cpp) and [backward-cpp](https://github.com/bombela/backward-cpp/tree/master)
+  * But these project ([icecream-cpp](https://github.com/renatoGarcia/icecream-cpp)) is under development, you maybe need to update related header for new features and fix bugs.
+* [ ] Support Multi-Process/Thread Printer to `/tmp/{pid}/{tid}/{fun_name}.log`
---
+++ csrc/printer/icecream-wrap.hpp
@@ -0,0 +1,67 @@
+// icecream-wrap.hpp
+#ifndef ICECREAM_WRAP_HPP
+#define ICECREAM_WRAP_HPP
+
+#include "icecream.hpp"
+
+#include <unistd.h>
+#include <thread>
+#include <sstream>
+#include <sys/syscall.h>
+
+// 定义一些有颜色的输出宏
+#define RESET   "\033[0m"
+#define BLACK   "\033[30m"      /* Black */
+#define RED     "\033[31m"      /* Red */
+#define GREEN   "\033[32m"      /* Green */
+#define YELLOW  "\033[33m"      /* Yellow */
+#define BLUE    "\033[34m"      /* Blue */
+#define MAGENTA "\033[35m"      /* Magenta */
+#define CYAN    "\033[36m"      /* Cyan */
+#define WHITE   "\033[37m"      /* White */
+#define GPT   "\033[94m"      /* gpt-blue */
+
+#define passBY(format, ...) { \
+    pid_t tid = syscall(SYS_gettid); /* 获取系统级 TID */ \
+    std::string thread_id_str = std::to_string(tid); \
+    std::string prefix = std::string(GREEN) + "\n----------------------------- " + format + " ----------------------------- " \
+                        + std::to_string(getpid()) + "/" + thread_id_str + "\n\n" +  std::string(RESET); \
+    IC_CONFIG.prefix(prefix.c_str()); \
+    IC(__VA_ARGS__); \
+    prefix = std::string(YELLOW) + "info " + std::to_string(getpid()) + "/" + thread_id_str + " | " + std::string(RESET); \
+    IC_CONFIG.prefix(prefix.c_str()); \
+}
+
+#define errorBY(format, ...) { \
+    pid_t tid = syscall(SYS_gettid); /* 获取系统级 TID */ \
+    std::string thread_id_str = std::to_string(tid); \
+    std::string prefix = std::string(RED) + "-------------- " + format + " -------------- " \
+                        + std::to_string(getpid()) + "/" + thread_id_str + "\n\n" +  std::string(RESET); \
+    IC_CONFIG.prefix(prefix.c_str()); \
+    IC(__VA_ARGS__); \
+    prefix = std::string(YELLOW) + "info " + std::to_string(getpid()) + "/" + thread_id_str + " | " + std::string(RESET); \
+    IC_CONFIG.prefix(prefix.c_str()); \
+}
+
+#define colorBY(color, format, ...) { \
+    pid_t tid = syscall(SYS_gettid); /* 获取系统级 TID */ \
+    std::string thread_id_str = std::to_string(tid); \
+    std::string prefix = std::string(color) + format + " " + std::to_string(getpid()) + "/" + thread_id_str  + " | " + std::string(RESET); \
+    IC_CONFIG.prefix(prefix.c_str()); \
+    IC(__VA_ARGS__); \
+    prefix = std::string(YELLOW) + "info " + std::to_string(getpid()) + "/" + thread_id_str + " | " + std::string(RESET); \
+    IC_CONFIG.prefix(prefix.c_str()); \
+}
+
+#define tIC(...) { \
+    pid_t tid = syscall(SYS_gettid); /* 获取系统级 TID */ \
+    std::string thread_id_str = std::to_string(tid); \
+    std::string prefix = std::string(YELLOW) + "info " + std::to_string(getpid()) + "/" + thread_id_str + " | " + std::string(RESET); \
+    IC_CONFIG.prefix(prefix.c_str()); \
+    IC(__VA_ARGS__); \ 
+}
+
+
+
+
+#endif // ICECREAM_WRAP_HPP
