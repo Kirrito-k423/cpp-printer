7472dfcce0c4701a086a00faf9b6c5eb451949b7
shaojiemike <shaojiemike@mail.ustc.edu.cn>
2024-11-28 15:28:01 +0800

    [script] add run.sh

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,5 @@
-./build/tests/test_printer
+logPath=./build/tests/test_printer.log
+
+./build/tests/test_printer |tee $logPath
+
+python ./scripts-py/parse_chrome_tracing.py --inputlogfile $logPath --output ./test.json
